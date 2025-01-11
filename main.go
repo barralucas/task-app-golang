b3733ae1026af3acfd1ad7d8c581c74de9cbaaac
Lucas Barra <lucas.trindade@funcionarios.automatizai.com.br>
2025-01-11 16:45:08 +0000

    Todos struct

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        string `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World in Go!")
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Hello, World!",
+		})
 	})
 
 	app.Listen(":3000")
