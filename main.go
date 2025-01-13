86b5ab0e75d2a000be6c529263753032f92cb6a7
Lucas Barra <lucas.trindade@funcionarios.automatizai.com.br>
2025-01-13 01:36:52 +0000

    feat: implement basic todo API with GET and POST endpoints

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -19,6 +21,26 @@ func main() {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"message": "Hello, World!",
 		})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(&todo); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Body is required",
+			})
+		}
+
+		todo.ID = strconv.Itoa(len(todos) + 1)
+		todos = append(todos, *todo)
+
+		return c.Status(fiber.StatusCreated).JSON(todos)
 	})
 
 	app.Listen(":3000")
