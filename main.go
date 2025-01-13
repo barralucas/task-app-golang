38bf2f880f35257687b32a49f15677e8002de503
Lucas Barra <lucastbarra@hotmail.com>
2025-01-13 23:18:47 +0000

    feat: implement basic todo API with create and update endpoints

--- main.go
+++ main.go
@@ -23,9 +23,10 @@ func main() {
 		})
 	})
 
+	// Create a ToDo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
-		if err := c.BodyParser(&todo); err != nil {
+		if err := c.BodyParser(todo); err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 				"error": err.Error(),
 			})
@@ -43,5 +44,20 @@ func main() {
 		return c.Status(fiber.StatusCreated).JSON(todos)
 	})
 
+	// Update a ToDo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if todo.ID == id {
+				todos[i].Completed = !todos[i].Completed
+				return c.Status(fiber.StatusOK).JSON(todos[i])
+			}
+		}
+
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Todo not found",
+		})
+	})
 	app.Listen(":4001")
 }
