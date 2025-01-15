7635bb779265162211bb57fb64c47d30ae9ce058
Lucas Barra <lucastbarra@hotmail.com>
2025-01-15 00:00:24 +0000

    feat: add godotenv library for environment variables management

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -15,12 +18,15 @@ type Todo struct {
 func main() {
 	app := fiber.New()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	todos := []Todo{}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "Hello, World!",
-		})
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(todos)
 	})
 
 	// Create a ToDo
@@ -66,7 +72,9 @@ func main() {
 		for i, todo := range todos {
 			if todo.ID == id {
 				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(fiber.StatusOK).JSON(todos)
+				return c.Status(fiber.StatusOK).JSON(fiber.Map{
+					"message": "Todo deleted successfully",
+				})
 			}
 		}
 
@@ -74,5 +82,6 @@ func main() {
 			"error": "Todo not found",
 		})
 	})
-	app.Listen(":4001")
+
+	app.Listen(":" + os.Getenv("PORT"))
 }
